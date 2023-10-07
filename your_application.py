a1f2540b2834fc483b5fac018193f6d8c0a7a93f
seannnnnn1017 <op23756778@gmail.com>
2023-10-07 11:50:25 +0800

    123

--- your_application.py
+++ your_application.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-your_application = Flask(__name__, template_folder='templates') # buliding application item
+your_application = Flask(__name__, template_folder='templates',static_folder='static') # buliding application item
 
 # building website home
 @your_application.route('/') #This function is used to respond to website connections.
 def index():
     return render_template("home.html")
 
-@your_application.route('/hello')
+@your_application.route('/hello') #other websit loaction
 def hello():
-    return render_template('hello.php')
+    data="hello data" #Create a variable 
+    return render_template('hello.php', data=data) #pass the variable to websit
 #start websit server
 your_application.run()
