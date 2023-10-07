799117086c799b4eba722920d5bde29c0cb35d0d
seannnnnn1017 <op23756778@gmail.com>
2023-10-07 13:36:19 +0800

    123

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates',static_folder='static') # buliding application item
 
 # building website home
@@ -12,6 +12,35 @@ def hello():
     return render_template('hello.php', data=data) #pass the variable to websit
 #start websit server
 
+@app.route('/form') 
+def test_form():
+    return render_template("test_form.html")
 
+@app.route('/user/<username>',methods=["GET","POST"])
+def get_user(username):
+    return "hello "+username
+
+@app.route('/data_get',methods=["GET","POST"])
+def test_data(): #http://127.0.0.1:5000/data_get?a=111&b=23
+    print(request.args) #use to get after url variable
+    print(request.args.get("a"),request.args.get("b"))
+
+    #print(request.headers)
+    print(request.headers.get('User-Agent'))
+
+    print(request.form.get('username'),request.form.get('password'))
+    return 'success'
+
+@app.route("/file_get") #getting file data
+def file_get():
+    #read file
+    data=[]
+    with open("./data/demo_data.txt") as fin:
+        for line in fin:
+            line=line[:-1]
+            times,number=line.split("	")
+            data.append((times,number))
+    #return html
+    return render_template("file_get.html",data=data)
 if __name__=="__main__":
     app.run()
