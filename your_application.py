dcc9fcc6710d41b0dd9c44d8617643243ae3fab4
seannnnnn1017 <op23756778@gmail.com>
2023-10-07 11:43:01 +0800

    123

--- your_application.py
+++ your_application.py
@@ -1,10 +1,10 @@
-from flask import Flask
-your_application = Flask(__name__) # buliding application item
+from flask import Flask, render_template
+your_application = Flask(__name__, template_folder='templates') # buliding application item
 
 # building website home
 @your_application.route('/') #This function is used to respond to website connections.
 def index():
-    return "Hello" #content
+    return render_template("home.html")
 
 #start websit server
 your_application.run()
